e7111bdddb97cfa9072e7d137ba29c34fd370116
quellpy <quellpy@yandex.ru>
2024-05-05 19:32:30 +0300

    Added config and toggle check leds

--- src/main.c
+++ src/main.c
@@ -11,16 +11,43 @@
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec greenled = GPIO_DT_SPEC_GET(LED_GREEN, gpios);
+
+int init_leds(void)
+{
+	int chk = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (chk != 0) {
+		printf("Failed to configure led");
+		return chk;
+	}
+	
+	chk = gpio_pin_configure_dt(&greenled, GPIO_OUTPUT_ACTIVE);
+	if (chk != 0) {
+		printf("Failed to configure green led");
+		return chk;
+	}
+	return 0;
+}
 int main(void)
 {
+	if (init_leds() != 0) {
+		return 1;
+	}
 	
-	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&greenled, GPIO_OUTPUT_ACTIVE);
-
 	while (1) {
-		gpio_pin_toggle_dt(&led);
+		int chk = gpio_pin_toggle_dt(&led);
+		if (chk != 0) {
+			printf("Failed to toggle led");
+			return 1;
+		}
+		
 		k_msleep(SLEEP_TIME_MS);
-		gpio_pin_toggle_dt(&greenled);
+
+		chk = gpio_pin_toggle_dt(&greenled);
+		if (chk != 0) {
+			printf("Failed to toggle green led");
+			return 1;
+		}
+
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
